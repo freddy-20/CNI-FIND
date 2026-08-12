04319e8329b4673e75aa98bcfe8a172f6314ea50
freddy-20 <freddysmth03@gmail.com>
2026-08-12 22:18:44 +0100

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+
+      <div className="text-center">
+
+        <h1 className="text-6xl font-bold">
+          404
+        </h1>
+
+        <p className="mt-4 text-gray-500">
+          Page introuvable.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
