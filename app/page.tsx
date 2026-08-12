241028c1c1866a499cdaa5d83de8b8d46e34772d
freddy-20 <freddysmth03@gmail.com>
2026-08-12 16:29:04 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/home/Hero";
+import HowItWorks from "@/components/home/HowItWorks";
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <HowItWorks />
+    </>
+  );
+}
