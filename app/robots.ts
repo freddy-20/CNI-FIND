75099e324df11facf0745bbfce09581c56a90181
freddy-20 <freddysmth03@gmail.com>
2026-08-12 22:07:28 +0100

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/admin",
+        "/api",
+      ],
+    },
+    sitemap:
+      "https://cnifind.cm/sitemap.xml",
+  };
+}
