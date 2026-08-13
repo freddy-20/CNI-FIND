e0c6733e5085cb6c2a0452fb43cffac8b6b736b8
freddy-20 <freddysmth03@gmail.com>
2026-08-13 01:21:27 +0100

    Create route.ts

---
+++ app/api/admin/delete-found/[id]/route.ts
@@ -0,0 +1,13 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  await prisma.foundDocument.delete({ where: { id } });
+
+  return NextResponse.redirect(new URL("/admin", request.url));
+}
