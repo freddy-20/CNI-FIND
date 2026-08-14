becc4b24f54b071f1aee2a498255389a54833357
freddy-20 <freddysmth03@gmail.com>
2026-08-14 06:54:17 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/Hero";
 import HowItWorks from "@/components/home/HowItWorks";
+import AnimatedBackground from "@/components/shared/AnimatedBackground";
 
 export default function HomePage() {
   return (
-    <>
+    <div className="relative">
+      <AnimatedBackground />
       <Hero />
       <HowItWorks />
-    </>
+    </div>
   );
 }
